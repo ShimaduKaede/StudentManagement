f35d37f5207f4f6a758da95802ac59f6ada569f7
LyleLD <ooi2372004@stu.o-hara.ac.jp>
2024-06-06 11:57:46 +0900

    School.javaを編集

--- src/bean/School.java
+++ src/bean/School.java
@@ -1,5 +1,23 @@
 package bean;
 
-public  School {
-    
+import java.io.Serializable;
+public class School implements Serializable {
+    private String cd;
+    private String name;
+
+    // ゲッター
+    public String getCd() {
+        return cd;
+    }
+    public String getName() {
+        return name;
+    }
+
+    // セッター
+    public void setCd(String cd) {
+        this.cd = cd;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
