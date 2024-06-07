3bd9f4edfc43e4a45dca7e185b47a9994f6f45d1
kazuki <ooi2372414@stu.o-hara.ac.jp>
2024-06-07 10:27:31 +0900

    been　subject.java　終わりましたのでコミットします。　矢野

--- src/bean/Subject.java
+++ src/bean/Subject.java
@@ -1,5 +1,32 @@
 package bean;
 
-public class Subject {
-    
+import java.io.Serializable;
+public class Subject implements Serializable {
+    private String subjectCd;      // 科目コード
+    private String subjectName;    // 科目名
+    private School school;          // 学校情報
+
+
+    // ゲッター
+    public String getSubjectCd() {
+        return subjectCd;
+    }
+    public String getSubjectName() {
+        return subjectName;
+    }
+    public School getSubjectSchool() {
+        return school;
+    }
+
+    // セッター
+    public void setSubjectCd(String subjectCd) {
+        this.subjectCd = subjectCd;
+    }
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+
+    }
+    public void Setsubject_School(School school) {
+        this.school = school;
+    }
 }
