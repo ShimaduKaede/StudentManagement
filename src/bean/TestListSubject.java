3aa52cb114ad324858ddd7bfc0be99fa0924f102
LyleLD <ooi2372004@stu.o-hara.ac.jp>
2024-06-07 12:38:44 +0900

    TestListSubject.java　途中経過保存

--- src/bean/TestListSubject.java
+++ src/bean/TestListSubject.java
@@ -1,5 +1,40 @@
 package bean;
 
-public class TestListSubject {
+import java.io.Serializable;
+public class TestListSubject implements Serializeble {
     
+    private int entYear;
+    private String studentNo;
+    private String studentName;
+    private String classNum;
+    private Map<integer,integer> testlistPoints;
+
+    TestListSubject() {
+    }
+    // ゲッター
+    public int getEntYear() {
+        return entYear;
+    }
+    public String getStudentNo() {
+        return studentNo;
+    }
+    public String getStudentName() {
+        return studentName;
+    }
+    public String getClassNum() {
+        return classNum;
+    }
+    public Map<Integer, Integer> getPoints() {
+        return points;
+    }
+
+    // セッター
+    public int setIntYear(int entYear) {
+        this.entYear = entYear;
+    }
+    public String setStudentNo(string studentNo) {
+        this.studentNo = studentNo;
+    }
+    public String
+
 }
