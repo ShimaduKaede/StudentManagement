517658d319cb122fe809f17e00ba84dc0483ee17
kazuki <ooi2372414@stu.o-hara.ac.jp>
2024-06-10 10:03:07 +0900

    変数を変えました。

--- src/bean/TestListStudent.java
+++ src/bean/TestListStudent.java
@@ -2,52 +2,52 @@ package bean;
 
 // `TestListStudent` クラスは、学生の科目に関する情報を保持するクラス。
 public class TestListStudent {
-    private String subjectName;  // 科目名を表すフィールド
-    private String subjectCd;    // 科目コードを表すフィールド
-    private int num;             // 数量を表すフィールド
-    private int point;           // 点数を表すフィールド
+    private String testListStudentName;  // 科目名を表すフィールド
+    private String testListStudentCd;    // 科目コードを表すフィールド
+    private int testListNum;             // 数量を表すフィールド
+    private int testListPoint;           // 点数を表すフィールド
 
     // コンストラクタ（省略可能）
 
     // ゲッターメソッドとセッターメソッド
 
     // `subjectName` フィールドの値を返す。
-    public String getSubjectName() {
-        return subjectName;
+    public String gettestListStudentName() {
+        return testListStudentName;
     }
 
     // `subjectName` フィールドに値を設定。
-    public void setSubjectName(String subjectName) {
-        this.subjectName = subjectName;
+    public void settestListStudentName(String testListStudentName) {
+        this.testListStudentName = testListStudentName;
     }
 
     // `subjectCd` フィールドの値を返す。
-    public String getSubjectCd() {
-        return subjectCd;
+    public String gettestListStudentCd() {
+        return testListStudentCd;
     }
 
     // `subjectCd` フィールドに値を設定。
-    public void setSubjectCd(String subjectCd) {
-        this.subjectCd = subjectCd;
+    public void settestListStudentCd(String testListStudentCd) {
+        this.testListStudentCd = testListStudentCd;
     }
 
     // `num` フィールドの値を返す。
-    public int getNum() {
-        return num;
+    public int gettestListNum() {
+        return testListNum;
     }
 
     // `num` フィールドに値を設定。
-    public void setNum(int num) {
-        this.num = num;
+    public void settestListNum(int testListNum) {
+        this.testListNum = testListNum;
     }
 
     // `point` フィールドの値を返す。
-    public int getPoint() {
-        return point;
+    public int gettestListPoint() {
+        return testListPoint;
     }
 
     // `point` フィールドに値を設定。
-    public void setPoint(int point) {
-        this.point = point;
+    public void settestListPoint(int testListPoint) {
+        this.testListPoint = testListPoint;
     }
 }
