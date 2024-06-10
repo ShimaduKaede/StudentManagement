72d1ca265380ee697cf42c21a91a839d92f6956e
TAHESHIICHIKI <ooi2372401@stu.o-hara.ac.jp>
2024-06-10 11:39:55 +0900

    ClassNum.javaを作成終了

---
+++ src/bean/ClassNum.java
@@ -0,0 +1,26 @@
+package bean;
+
+// Serializableインターフェースを実装してBeanを作成する
+import java.io.Serializable;
+
+public class ClassNum implements Serializable {
+    private String schoolCd;    // 学校コード   
+    private String classNum;    // クラス番号
+
+    // 学校コードを取得(ゲット)する
+    public String getScoolCd() {
+        return schoolCd;
+    }
+    // 学校コードを設定(セット)する
+    public void setSchoolCd(String schoolCd) {
+        this.schoolCd;
+    }
+    // クラス番号を取得(ゲット)する
+    public String getClassNum() {
+        return classNum;
+    }
+    // クラス番号を設定(セット)する
+    public void setClassNum(String classNum) {
+        this.classNum;
+    }
+}
