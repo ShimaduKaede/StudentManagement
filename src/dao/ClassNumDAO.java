a85a3cf3071b6677330565a776c9c9fde7d7c453
TAHESHIICHIKI <ooi2372401@stu.o-hara.ac.jp>
2024-06-11 10:01:29 +0900

    ClassNumDAO.java作成終了

--- src/dao/ClassNumDAO.java
+++ src/dao/ClassNumDAO.java
@@ -7,23 +7,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import bean.School;
-import bean.Student;
 public class ClassNumDAO {
 
 	// セッションのユーザーデータ(学校コード)から
 	// 所属している学校のクラスデータ(学校コード,クラス番号)を取得するメソッド
-	public List<String> filter(String schoolCd) throws Exception {
-		List<String> classNumList=new ArrayList<>();
+	public List<String> filter(School school) throws Exception {
+		List<String> classNumList=new ArrayList<>();	// 戻り値で使用する空Listを作成
+		String schoolCd = school.getSchoolCd();			// 学校コードを変数schoolCdに設定
 
 		Connection con=getConnection();
-
 		PreparedStatement st=con.prepareStatement(
-			"SELECT * FROM CLASS_NUM WHERE SCHOOL_CD = schoolCd");
+			"SELECT SCHOOL_CD,CLASS_NUM FROM CLASS_NUM WHERE SCHOOL_CD = schoolCd");
 		ResultSet rs=st.executeQuery();
 
 		while (rs.next()) {
-			classNumList.add(rs.getString("school_cd"));
-			classNumList.add(rs.getString("class_num"));
+			classNumList.add(rs.getString("SCHOOL_CD"));
+			classNumList.add(rs.getString("CALSS_NUM"));
 		}
 
 		st.close();
@@ -31,8 +30,5 @@ public class ClassNumDAO {
 
 		return classNumList;
 	}
-
-
-
 
 }
