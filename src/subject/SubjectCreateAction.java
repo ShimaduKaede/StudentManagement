e5be26e108453e3d4dc3ff6d61ac453640d53579
kazuki <ooi2372414@stu.o-hara.ac.jp>
2024-06-13 12:41:07 +0900

    終了

--- src/subject/SubjectCreateAction.java
+++ src/subject/SubjectCreateAction.java
@@ -8,30 +8,32 @@ import dao.SubjectDAO;
 import tool.Action;
 
 public class SubjectCreateAction extends Action {
-	public String execute(
-		HttpServletRequest request, HttpServletResponse response
-	) throws Exception {
-		// ユーザーからの入力値を受け取る
-		int subjectcd=Integer.parseInt(request.getParameter("SubjectCd"));
-		String name=request.getParameter("SubjectName");
-		int courseid=Integer.parseInt(request.getParameter("CourseId"));
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // ユーザーからの入力値を受け取る
+        String schoolCd = request.getParameter("SchoolCd");
+        String subjectCd = request.getParameter("SubjectCd");
+        String subjectName = request.getParameter("SubjectName");
 
-		// Subjectビーンに設定
-		Subject subject=new Subject();
-		subject.setSubjectCd(subjectcd);
-		subject.setSubjectName(name);
-		subject.setCourseId(courseid);
-		// SubjectDAOインスタンスを生成
-		SubjectDAO dao=new SubjectDAO();
-		// SubjectDAOのstudentInsertメソッドを実行してデータベースに登録
-		int line = dao.Subject(subject);
+        // Subjectビーンに設定
+        Subject subject = new Subject();
+        subject.setSchoolCd(schoolCd);
+        subject.setSubjectCd(subjectCd);
+        subject.setSubjectName(subjectName);
 
-		// lineが0でなければ登録成功
-		if (line != 0) {
-			request.setAttribute("message", "登録しました");
-		} else {
-			request.setAttribute("message", "登録に失敗しました");
-		}
-		return "subject_create.jsp";
-	}
+        // SubjectDAOインスタンスを生成
+        SubjectDAO dao = new SubjectDAO();
+
+        // SubjectDAOのsaveメソッドを実行してデータベースに登録
+        boolean success = dao.save(subject);
+
+        // 登録成功かどうかでメッセージを設定
+        if (success) {
+            request.setAttribute("message", "登録しました");
+        } else {
+            request.setAttribute("message", "登録に失敗しました");
+        }
+
+        // 遷移先のJSPを返す
+        return "subject_create_done.jsp";
+    }
 }
