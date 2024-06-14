525b5debaeac84f448cd1a5c23add02db031fa28
LyleLD <ooi2372004@stu.o-hara.ac.jp>
2024-06-14 12:17:36 +0900

    login昨日作成（Action)

--- src/account/LoginAction.java
+++ src/account/LoginAction.java
@@ -1,5 +1,37 @@
-package account;
-
-public class LoginAction {
-    
+package account ;
+ 
+import java.io.IOException;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+// このURLパターンはhttp://localhost:8080/kouka2で実行される
+@WebServlet(urlPatterns={"/login/loginaction"})
+public class LoginAction extends HttpServlet {
+    public void doGet (
+        HttpServletRequest request, HttpServletResponse response
+    ) throws ServletException, IOException {
+ 
+ 
+        try {
+ 
+            // 意図的に例外を発生させる処理（普段はつかわない）
+             //if (true) {
+             //       throw new RuntimeException("テスト用の予期せぬエラー");
+             // }
+ 
+            // login.jspへフォワードするだけ
+            request.getRequestDispatcher("/student/login/login.jsp")
+                .forward(request, response);
+        }catch(Exception e){
+         // エラーメッセージを設定してエラーページに遷移
+           request.setAttribute("message", "エラーが発生しました。");
+           request.getRequestDispatcher("/student/subject_error.jsp")
+            .forward(request, response);
+ 
+    }
+    }
 }
