b7cbb6b41c5754b799b347519eeccf39844e8163
TAHESHIICHIKI <ooi2372401@stu.o-hara.ac.jp>
2024-06-14 10:25:47 +0900

    StudentListAction.java作成中

--- src/student/StudentListAction.java
+++ src/student/StudentListAction.java
@@ -1,5 +1,32 @@
 package student;
 
-public class StudentListAction {
-    
+import tool.Action;
+import tool.Utl;
+public class StudentListAction extends Action {
+	public String execute(
+		HttpServletRequest request, HttpServletResponse response
+	) throws Exception {
+
+        // sessionデータを取得
+        // セッションからユーザーデータを取得
+
+		// getParameterメソッドででえーたを受け取る
+		// name属性で指定した名前で受け取る
+		String data1=request.getParameter("data1");
+		String data2=request.getParameter("data2");
+
+		// data1を加工
+        // sessionデータを渡す
+        // セションのユーザーデータから、ユーザーが所属している学校の生徒一覧用データを取得
+
+		// setAttributeメソッドでホールスタッフにデータを渡す
+		// 第１引数にjspで受け取るときの名前
+		// 第２引数にjspに渡すデータを指定する
+		request.setAttribute("data1", data1);
+		request.setAttribute("data2", data2);
+
+		// FrontControllerを使用しているためreturn文でフォワードできる
+		return "student_list.jsp";
+	}
 }
+
