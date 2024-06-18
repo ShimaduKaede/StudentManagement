1687ae9f4293446653e8f2769879beff9c9a8351
LyleLD <ooi2372004@stu.o-hara.ac.jp>
2024-06-18 10:44:33 +0900

    LoginExecuteAction編集

--- src/account/LoginExecuteAction.java
+++ src/account/LoginExecuteAction.java
@@ -1,42 +1,38 @@
 package account;
- 
-import java.util.List;
- 
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
- 
+
 import bean.Teacher;
 import dao.TeacherDAO;
 import tool.Action;
- 
+
 public class LoginExecuteAction extends Action {
     public String execute(
         HttpServletRequest request, HttpServletResponse response
     ) throws Exception {
-        try{
- 
+        try {
             HttpSession session = request.getSession();
- 
+
             String teacherId = request.getParameter("teacherId");
             String teacherPassword = request.getParameter("teacherPassword");
- 
+
             TeacherDAO dao = new TeacherDAO();
-            List<Teacher> customer = dao.login(teacherId, teacherPassword); // ログインIDとパスワードを使って検証する
- 
-            if (customer != null) {
-                Teacher teacher = customer.get(0);
+            Teacher teacher = dao.login(teacherId, teacherPassword); // ログインIDとパスワードを使って検証する
+
+            if (teacher != null) {
                 session.setAttribute("teacher", teacher);
- 
                 return "/student/menu.jsp"; // ログイン成功時のリダイレクト先
+            } else {
+                request.setAttribute("message", "ログインIDまたはパスワードが違います。");
+                return "/student/login.jsp"; // ログイン失敗時のリダイレクト先
             }
- 
-            
-        }catch(Exception e){
-         // エラーメッセージを設定してエラーページに遷移
-           request.setAttribute("message", "エラーが発生しました。");
-           return "student/error.jsp";
- 
-    }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            request.setAttribute("message", "エラーが発生しました。");
+            return "/student/error.jsp"; // エラーページに遷移
+        }
     }
 }
