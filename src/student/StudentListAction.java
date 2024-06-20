7e9a10cb8cc23d177178e4ede8a4925afcf11b90
TAHESHIICHIKI <ooi2372401@stu.o-hara.ac.jp>
2024-06-20 09:57:57 +0900

    ひとまず、分岐

--- src/student/StudentListAction.java
+++ src/student/StudentListAction.java
@@ -8,20 +8,12 @@ public class StudentListAction extends Action {
 	public String execute(
 		HttpServletRequest request, HttpServletResponse response
 	) throws Exception {
-		// Sessionの開始
-		HttpSession session=request.getSession();
-        // sessionデータを取得
-		String data1=session.getId()
-        // セッションからユーザーデータを取得
 
-        // sessionデータを渡す
-        // セションのユーザーデータから、ユーザーが所属している学校の生徒一覧用データを取得
+		Teacher t=new Teacher();
+		Utl tool = new Utl();
+		t = tool.getUser(request);
 
-		// setAttributeメソッドでホールスタッフにデータを渡す
-		// 第１引数にjspで受け取るときの名前
-		// 第２引数にjspに渡すデータを指定する
-		request.setAttribute("data1", data1);
-
+		request.setAttribute("user", t);
 		// FrontControllerを使用しているためreturn文でフォワードできる
 		return "student_list.jsp";
 	}
