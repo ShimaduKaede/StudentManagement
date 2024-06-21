0dcbc1d220123c1f58934f60fa0f194ebb9fe520
TAHESHIICHIKI <ooi2372401@stu.o-hara.ac.jp>
2024-06-21 12:50:20 +0900

    StudentDAO修正中

--- src/dao/StudentDAO.java
+++ src/dao/StudentDAO.java
@@ -7,7 +7,81 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import bean.Student;
+import bean.School;
 public class StudentDao extends Dao {
+    String baseSql; // おそらくSQL文を確保する変数
+
+    // getメソッド
+    // 引数：sutudentNo
+    // 戻り値：Student
+    // 学生をIDで取得するメソッド
+    public Student get(String studentNo) throws  Exception {
+        student=new Student();	// 戻り値で使用するstudentを作成
+    
+        Connection con=getConnection(); // DBへの接続
+        PreparedStatement st=con.prepareStatement(
+            "SELECT NO,NAME,ENT_YEAR,CLASS_NUM,IS_ATTEND,SCHOOL_CD FROM STUDENT WHERE SCHOOL_CD = schoolCd");
+        ResultSet rs=st.executeQuery();
+
+        st.close(); 
+		con.close();    // DB切断
+
+        return student;
+    }
+
+    // postFilterメソッド
+    // セッションのユーザーデータから、
+    // ユーザが所属している学校の生徒一覧用データを取得
+    // 引数：ResultSet型のrSet,School型のschool
+    // 戻り値：List<Student>
+    public List<Student> postFilter(ResultSet rSet,School school) throws Exception {
+		List<Student> studentlist=new ArrayList<>();    // 戻り値で使用するstudentlistを作成
+		String schoolCd = school.getSchoolCd();			// 学校コードを変数schoolCdに設定
+        
+        Connection con=getConnection(); // DBへの接続
+        PreparedStatement st=con.prepareStatement(
+            "SELECT NO,NAME,ENT_YEAR,CLASS_NUM,IS_ATTEND,SCHOOL_CD FROM STUDENT WHERE SCHOOL_CD = schoolCd");
+        ResultSet rs=st.executeQuery();
+// -------------------- ここまで --------------------
+		while (rs.next()) {
+			//科目Beanをインスタンス化して情報をセット
+			Subject subject=new Subject();
+			subject.setSchoolCd(rs.getStriing("SCHOOL_CD"));
+			subject.setSubjectCd(rs.getString("CD"));
+			subject.setSubjectName(rs.getString("NAME"));
+
+            subjectList.add(subject);
+		}
+
+        st.close(); 
+		con.close();    // DB切断
+
+		return subjectList;
+    }
+
+    // filter1メソッド
+    // 引数：School型のschool,int型のentYear,String型のclassNum,boolean型のisAttend
+    // 戻り値：List<Student>
+
+    // filter2メソッド
+    // 引数：School型のschool,int型のentYear,boolean型のisAttend
+    // 戻り値：List<Student>
+
+    // filter3メソッド
+    // 学生IDを取得 → 学生の詳細データを取得
+    // 引数：School型のschool,boolean型のisAttend
+    // 戻り値：List<Student>
+
+    // saveメソッド
+    // 入力された値をDBに保存する
+    // 引数：Student型のstudent
+    // 戻り値：boolean
+
+
+
+
+
 
     // 学生をIDで取得するメソッド
     public Student get(String studentNo) throws SQLException {
