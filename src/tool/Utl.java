65d236be53058e5a3366128d1874f14d66b0d3aa
TAHESHIICHIKI <ooi2372401@stu.o-hara.ac.jp>
2024-06-21 09:46:23 +0900

    Utl.java修正：getUserメソッドに仮データ入力してます

--- src/tool/Utl.java
+++ src/tool/Utl.java
@@ -14,17 +14,27 @@ public class Utl extends HttpServlet {
     // ログインしたユーザー情報をセションから取得する(Teacher型)メソッド
     public Teacher getUser(HttpServletRequest request
 	) throws ServletException, IOException {
-        // セッションの開始
-		HttpSession session=request.getSession();	
         // 格納用のTeacher型のtをインスタンス化
 	    Teacher t=new Teacher();
+
+        // ログインが出来るまで、ひとまず、コメントアウト
         // 以下でセッション情報を変数tにセット
-        t.setTeacherId(session.getAttribute('teacherId'));
-        t.setTeacherPassword(sessiion.getAttribute("password"));
-        t.setTeacherName(session.getAttribute("teacherName"));
+        // t.setTeacherId(session.getAttribute('teacherId'));
+        // t.setTeacherPassword(sessiion.getAttribute("password"));
+        // t.setTeacherName(session.getAttribute("teacherName"));
+        // School school = new School();
+        // school.setSchoolCd(session.getAttribute("schoolCd"));
+        // school.setSchoolName(session.getAttribute("schoolName"));
+        // t.setSchool(school);
+
+        // ログインが出来るまで、ひとまず、サンプルデータを仮入力してます
+        // 以下でセッション情報を変数tにセット
+        t.setTeacherId("admin");
+        t.setTeacherPassword("password"));
+        t.setTeacherName("大原花子");
         School school = new School();
-        school.setSchoolCd(session.getAttribute("schoolCd"));
-        school.setSchoolName(session.getAttribute("schoolName"));
+        school.setSchoolCd("oom");
+        school.setSchoolName("大宮校");
         t.setSchool(school);
 
         return t;   // 変数t:Teacher型の情報を返す
@@ -32,8 +42,6 @@ public class Utl extends HttpServlet {
     // クラス番号情報をセッションにセットする
     public void setClassNumSet(HttpServletRequest request
     ) throws ServletException, IOException {
-        // セッションの開始
-		HttpSession session=request.getSession();	
         // 以下でセッション情報にセット
         session.setAttribute("schoolCd",request.schoolCd);
         session.setAttribute("classNum",request.classNum);
@@ -41,16 +49,12 @@ public class Utl extends HttpServlet {
     // 入学年度をセッションにセットする
     public void setEntYearSet(HttpServletRequest request
 	) throws ServletException, IOException {
-        // セッションの開始
-		HttpSession session=request.getSession();	
         // 以下でセッション情報にセット
         session.setAttribute("entYear",request.entYear);
     }
     // 科目情報をセッションにセットする
     public void setSubjects(HttpServletRequest request
 	) throws ServletException, IOException {
-        // セッションの開始
-		HttpSession session=request.getSession();	
         // 以下でセッション情報にセット
         session.setAttribute("subjectCd",request.subjectCd);
         session.setAttribute("subjectName",request.subjectName);
