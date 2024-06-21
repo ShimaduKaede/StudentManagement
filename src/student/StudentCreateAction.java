b35cc97c0d303358551c4869334aa52df35358ba
iwaboko154cm <ooi2372402@stu.o-hara.ac.jp>
2024-06-21 13:49:01 +0900

    StudentCreateAction.java

--- src/student/StudentCreateAction.java
+++ src/student/StudentCreateAction.java
@@ -5,27 +5,16 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import bean.School;
-import bean.Teacher;
 import dao.ClassNumDAO;
 import tool.Action;
-// import tool.Utl;
 
 public class StudentCreateAction extends Action {
 	public String execute(
 		HttpServletRequest request, HttpServletResponse response
 	) throws Exception {
 
-		// セッションデータからログインしているユーザーデータを取得（A中学校という情報を取得）
-		// Teacher teacher = Utl.getUser();
-		Teacher teacher = new Teacher(); //Teacherビーンをインスタンス化
-		teacher.setTeacherId("1");
-		teacher.setTeacherName("kayano");
-		teacher.setTeacherPassword("1234");
-		School school = new School();
-		school.setSchoolCd("ooi");
-		school.setSchoolName("大分校");
-		teacher.setSchool(school);
+
+
 
 		// フォワード先のクラスのSELECTボックスで使用するために
 		// classテーブルからユーザーが所属している学校のクラス一覧を取得（A中学校で検索）
