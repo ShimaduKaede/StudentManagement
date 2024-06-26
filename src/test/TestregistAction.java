0f45ae31deb9054433405440e04cc1c0688f550a
LyleLD <ooi2372004@stu.o-hara.ac.jp>
2024-06-26 11:50:18 +0900

    TestRegistAction変更途中

--- src/test/TestregistAction.java
+++ src/test/TestregistAction.java
@@ -1,5 +1,42 @@
 package test;
 
-public class TestregistAction {
-    
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.Teacher;
+import dao.ClassNumDAO;
+import tool.Action;
+import tool.Utl;
+
+public class TestRegistAction extends Action {
+	public String execute(
+		HttpServletRequest request, HttpServletResponse response
+	) throws Exception {
+
+		// Utlクラスをインスタンス化
+		Utl utl = new Utl();
+		// ログインしているTeacherをセッション情報から取得する
+		Teacher teacher = utl.getUser(request);
+
+		// フォワード先のクラスのSELECTボックスで使用するために
+		// classテーブルからユーザーが所属している学校のクラス一覧を取得（A中学校で検索）
+		ClassNumDAO dao = new ClassNumDAO(); // ClassNumDAOを呼び出す
+		List<String> classes = dao.filter(teacher.getSchool());
+		System.out.println(classes);
+		// 取得したクラス一覧をjsp側に渡すためにセットする 第1引数にjspで使うときの名前を設定 第2引数に渡すデータを指定
+		request.setAttribute("classes", classes);
+		
+		// フォワード先の科目のSELECTボックスで使用するために
+		// subjectテーブルからユーザーが所属している学校のクラス一覧を取得 
+		SubjectDAO dao = new SubjectDAO();
+		List<String> subjects = dao.filter(teacher.getSchool());
+		System.out.println(subjects);
+		// 取得した科目一覧をjsp側に渡すためにセットする 第一引数にjspで使うときの名前を設定 第二引数に渡すデータを指定
+		request.setAttribute("subjects", subjects);
+
+		// FrontControllerを使用しているためreturn文でフォワードできる
+		return ".jsp";
+	}
 }
