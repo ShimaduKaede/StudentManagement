c093abe33173bc57eacbadfa64517400339a3862
TAHESHIICHIKI <ooi2372401@stu.o-hara.ac.jp>
2024-06-28 11:29:58 +0900

    SubjectDeleteActionひとまず終了

---
+++ src/subject/SubjectDeleteDoneAction.java
@@ -0,0 +1,60 @@
+package subject ;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import tool.Action;
+
+public class SubjectDeleteDoneAction extends Action {
+	public String execute(
+		HttpServletRequest request, HttpServletResponse response
+	) throws Exception {
+
+        // getJspNameメソッド
+        // 引数：なし
+        // 戻り値：String
+        public String getJspName(){
+            String jspName;
+
+            return jspName; 
+        }
+
+        // getSuccessUrlメソッド
+        // 引数：なし
+        // 戻り値：String
+        public String getSuccessUrl(){
+            String successUrl;
+
+            return successUrl;
+        }
+
+        // setRequestDataメソッド
+        // 引数：HttpServletRequest型のrequest
+        // 戻り値：なし
+        public void setRequestData(HttpServletRequest request){
+
+        }
+
+        // getメソッド
+        // 引数：HttpServletRequest型のrequest,HttpServletResponse型のresponse
+        // 戻り値：String
+        public String get(HttpServletRequest request, HttpServletResponse response){
+            String rtn;
+
+            return rtn;
+        }
+
+        // postメソッド
+        // 引数：HttpServletRequest型のrequest,HttpServletResponse型のresponse
+        // 戻り値：String
+        public String post(HttpServletRequest request, HttpServletResponse response){
+            String rtn;
+
+            return rtn;
+        }
+    }
+}
