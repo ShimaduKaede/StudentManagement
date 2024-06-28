921c9a0d99f7db18c46e2cf972251fbd9e1e482e
iwaboko154cm <ooi2372402@stu.o-hara.ac.jp>
2024-06-28 15:17:07 +0900

    StudentCreate.action.java一部修正

--- src/dao/ClassNumDAO.java
+++ src/dao/ClassNumDAO.java
@@ -14,7 +14,7 @@ public class ClassNumDAO extends DAO{
 	public List<String> filter(School school) throws Exception {
 		List<String> classNumList=new ArrayList<>();	// 戻り値で使用する空Listを作成
 		String schoolCd = school.getSchoolCd();			// 学校コードを変数schoolCdに設定
-
+		System.out.println("DAO内："+ schoolCd);
 		Connection con=getConnection();
 		PreparedStatement st=con.prepareStatement(
 			"SELECT CLASS_NUM FROM CLASS_NUM WHERE SCHOOL_CD = ?");
