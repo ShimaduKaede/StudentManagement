445fffcfd71801f63502db6b71b1013dded10c31
iwaboko154cm <ooi2372402@stu.o-hara.ac.jp>
2024-07-01 10:24:10 +0900

    StudentCreateExecuteActionを微修正

--- src/student/StudentCreateExecuteAction.java
+++ src/student/StudentCreateExecuteAction.java
@@ -4,8 +4,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import bean.Student;
+import bean.Teacher;
 import dao.StudentDAO;
 import tool.Action;
+import tool.Utl;
 
 public class StudentCreateExecuteAction extends Action {
 	public String execute(
@@ -13,8 +15,10 @@ public class StudentCreateExecuteAction extends Action {
 	) throws Exception {
 
 		// Utlを呼び出す
+		Utl utl = new Utl();
 
 		// UtlのgetUser()でログインユーザーを呼び出す
+		Teacher user = utl.getUser(request);
 
 		// student_create.jspから送られてきたデータを受け取る jsp側のname属性で指定された名前で受け取る
 		// String name = request.getAttribute("kayano");
@@ -26,13 +30,14 @@ public class StudentCreateExecuteAction extends Action {
 		// 受け取ったデータをStudentDAOのsaveメソッドで渡すためにStudentビーンをインスタンス化
 		// Studentのセッターメソッドを使用して、送られてきたデータをビーンにセット
         Student student = new Student();
-        
+
         student.setStudentName(name);
         student.setEntYear(entYear);
         student.setStudentNo(studentNo);
         student.setClassNum(classNum);
-        // student.schoolCdにログインユーザーのschoolCdをセット
 
+        // student.schoolCdにログインユーザーのschoolCdをセット
+        student.setSchoolCd(user.getSchoolCd());
 
 		// 受け取ったデータを登録するためにStudentDAOを呼び出す
         StudentDAO dao = new StudentDAO();
