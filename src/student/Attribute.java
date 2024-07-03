27a96dc940a0e98452f799a630ed63fd4b045405
TAHESHIICHIKI <ooi2372401@stu.o-hara.ac.jp>
2024-07-03 10:51:26 +0900

    Attribute.javaテスト中

---
+++ src/student/Attribute.java
@@ -0,0 +1,47 @@
+package student;
+
+import java.io.FileInputStream; // ファイルを読み込むためのFileInputStreamクラス
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Properties;    // プロパティファイルを扱うためのPropertiesクラス
+
+import javax.servlet.ServletContext;    // サーバ上の物理パスを取得するためのServletcontextインターフェース
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+import tool.Page;
+
+@WebServlet(urlPatterns={"/student/attribute"})
+public class Attribute extends HttpServlet {
+    public void doGet (
+        HttpServletRequest request, HttpServletResponse response
+    ) throws ServletException, IOException {
+        PrintWriter out=response.getWriter();
+        Page.header(out);
+
+        // getServletContextメソッドによりServletContextオブジェクトを取得
+        ServletContext context=getServletContext();
+        // 設定ファイルのサーバ上のパスを取得
+        // getRealPathメソッドの引数にはコンテキストルートを起点とするパスを指定
+        String path=context.getRealPath("/WEB-INF/setting.txt");
+        // 設定ファイルを開く
+        FileInputStream in=new FileInputStream(path);
+        // Propertiesクラスのインスタンスを生成
+        Properties p=new Properties();
+        // loadメソッドを使ってファイルを読み込む
+        p.load(in);
+        in.close();
+        // Propertiesオブジェクトに保存された名前の一覧を取得する
+        // stringPropertyNamesメソッドを使用
+        for (String name : p.stringPropertyNames()) {
+            // 設定ファイルから名前と値の組を1つ1つ取り出して
+            // setAttributeメソッドで属性に設定
+            context.setAttribute(name,p.getProperty(name));
+        }
+        out.println("アプリケーション属性を設定しました。");
+
+        Page.footer(out);
+    }
+
+}
