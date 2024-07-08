ee8877bde558dcabda611a18d5e7bd3023122124
kazuki <ooi2372414@stu.o-hara.ac.jp>
2024-07-08 10:22:05 +0900

    科目テストに移ります

--- src/subject/SubjectCreateExecuteAction.java
+++ src/subject/SubjectCreateExecuteAction.java
@@ -35,13 +35,20 @@ public class SubjectCreateExecuteAction extends Action {
         // 登録成功かどうかでメッセージを設定
         if (success) {
             request.setAttribute("message", "登録しました");
+
         } else {
-            request.setAttribute("message", "登録に失敗しました");
+            request.setAttribute("message", "登録に失敗しました: 重複する科目コードが存在します");
+            // 遷移先のJSPを返す
+            return "/student/error.jsp";
+
+
+
         }
 
         // 遷移先のJSPを返す
         return "subject_create_done.jsp";
     }
+
 }
 
 
